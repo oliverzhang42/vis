4ba2c90643cb53c2cd69823d8ed5ba190e161d52
Oliver <oliver.zhang.42@gmail.com>
2019-07-02 10:17:23 -0700

    Cleaned up visualize into a function and updated README

--- README.md
+++ README.md
@@ -7,23 +7,57 @@ Currently the tools accepts 5 arguments.
 --model (str): path to neural network
 --unprocessed_img (str): path to unprocessed img (jpg or png)
 --preprocessed_img (str): path to preprocessed img (Must be .npy file)
---vis (str): Either 'cam' (heatmap) or 'saliency' 
+--vis (str): Either 'cam' (heatmap), 'shap', 'integrated_grad', or 'saliency' 
 --conv_layer (int): Optional argument, used if you chose 'cam'. The index of the last conv2d layer in the model.
+--background (str): Optional argument, used if you chose 'integrated_grad' or 'shap'. Path to the "background image"
 ```
 
-Note that --conv_layer only comes into play if your last pooling
+#### Notes on arguments:
+```--conv_layer``` only comes into play if your last pooling
 layer is GlobalAveragPooling. This is the case for ResNet but not VGG.
+
+```--background``` only is necessary for shap or integrated_grad. Basically, these methods require
+a base image to compare against. This 'base image' should be an image which the model is uncertain about.
+(For resnet_ppg, a white image is a bad background because the model is confident that the PPG is bad.) 
+Also note that the background image must be preprocessed like all other images.
 
 ### How it works
 
-All my code is built upon https://github.com/raghakot/keras-vis. 
+My code is built upon the keras-vis library, the shap library, and the Integrated-Gradients library. 
 
-Saliency comes from the papers:
+```
+keras vis: https://github.com/raghakot/keras-vis
+shap: https://github.com/slundberg/shap
+Integrated-Gradients: https://github.com/ankurtaly/Integrated-Gradients
+```
+
+Saliency is implemented by keras-vis and comes from the papers:
 
 1. https://arxiv.org/abs/1312.6034 (vanilla saliency)
 2. https://arxiv.org/abs/1412.6806 (discusses guided saliency p.7)
 
-CAM comes from the papers:
+CAM is implemented by keras-vis and comes from the papers:
 
 1. https://arxiv.org/abs/1512.04150 (basic CAM with limited use case.)
 2. https://arxiv.org/abs/1610.02391 (generalization of CAM)
+
+Shap is implemented by the shap library and comes from the papers:
+
+(Note: Shap is actually a unified framework of different visualization methods. I'm actually using DeepLIFT, 
+a small part of the framework)
+
+1. https://arxiv.org/abs/1704.02685 (DeepLIFT, the subsection of shap that is used)
+2. https://arxiv.org/abs/1705.07874 (general shap)
+
+integrated_grad is implemented by the Integrated-Gradients library and comes from the paper:
+
+1. https://arxiv.org/abs/1703.01365
+
+### Dependencies Required:
+```
+numpy 1.16.4
+matplotlib 3.1.0
+keras 2.2.4
+keras-vis 0.4.1
+shap 0.29.2
+```
--- visualize.py
+++ visualize.py
@@ -1,3 +1,5 @@
+# TODO: Make sensitivity an argument
+
 import argparse
 from keras import activations
 from keras.models import load_model
@@ -11,124 +13,137 @@ from vis.visualization import visualize_saliency, overlay, visualize_cam
 from keras import backend as K
 import shap
 
-plt.rcParams['figure.figsize'] = (15, 10)
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('model', type=str, help='path of keras model')
-parser.add_argument('unprocessed_img', type=str, help='path of raw image.')
-parser.add_argument('preprocessed_img', type=str, help='path of image. Must be npy file and preprocessed.')
-parser.add_argument('vis', type=str, default='saliency', help='either CAM or saliency')
-parser.add_argument('--conv_layer', type=int, default=None,
-                    help='Used for CAM. Index of last convolutional layer')
-parser.add_argument('--background', type=str, help='path of "background" image')
-parser.add_argument('--show', type=bool, default=True, help='whether to display the image')
-# TODO: There seems to be a bug with show always showing.
+def visualize(img, unprocessed_img, vis, name, conv_layer=None, background=None, show=False):
+    batch = np.array([img])
 
-args = parser.parse_args()
+    # Load the model
+    model = load_model(args.model)
+    
+    # Make predictions (will be used later)
+    pred = model.predict(np.array([img]))[0]
+    
+    # Swap softmax with linear
+    model.layers[-1].activation = activations.linear
+    model = utils.apply_modifications(model)
+    
+    # Get visualization of last layer
+    layer_idx = -1
+    visualizations = []
+    neurons = model.layers[layer_idx].output_shape[1]
+    
+    # For each output option, visualize which inputs effect it.
+    
+    if vis == 'cam':
+        for i in range(neurons):
+            grads = visualize_cam(model, layer_idx, filter_indices=i,
+                                  seed_input=img, penultimate_layer_idx=conv_layer,
+                                  backprop_modifier=None)
+            # Lets overlay the heatmap onto original image.
+            jet_heatmap = np.uint8(cm.jet(grads)[..., :3] * 255)
+            visualizations.append(overlay(jet_heatmap, unprocessed_img, alpha=0.5))
+    elif vis == 'saliency':
+        for i in range(neurons):
+            visualizations.append(visualize_saliency(model, layer_idx, backprop_modifier='guided',
+                                                     filter_indices=i, seed_input=img))
+    elif vis == 'shap':
+        # select a set of background examples to take an expectation over
+        # NOTE: Not sure what background image to use, an expectation or a white image.
+        # Trying white image for now.
+        assert 'background' in vars(), "Shap uses a background image"
+        if len(background.shape) == 3:
+            background = np.array([background])
+    
+        # explain predictions of the model on four images
+        e = shap.DeepExplainer(model, background)
+    
+        shap_values = e.shap_values(batch)
+    
+        shap_values[0] = shap_values[0][0].astype('float32')
+        shap_values[1] = shap_values[1][0].astype('float32')
+    
+        # normalize the arrays
+        # shap_values = np.clip(shap_values, 0, np.max(shap_values))
+        shap_values = np.max(shap_values, -1)
+    
+        # plot the feature attributions
+        visualizations = shap_values
+    
+    elif vis == 'integrated_grad':
+        for i in range(neurons):
+            assert 'background' in vars(), "Integrated Gradients uses a background image"
 
-assert args.vis in ['cam', 'saliency', 'shap', 'integrated_grad'], \
-    "vis must be cam, shap, integrated_grad or saliency!"
+            def get_pred_and_grad(input, class_index):
+                input = np.array(input)
+                pred = model.predict(input)
+                fn = K.function([model.input], K.gradients(model.output[:, class_index], model.input))
+    
+                grad = fn([input])[0]
+    
+                return pred, grad
+    
+            attributions = integrated_gradients(
+                img,
+                i,
+                get_pred_and_grad,
+                baseline=background,
+                steps=50
+            )
+    
+            grad = attributions[0]
+    
+            # removing all negative gradients
+            grad = np.clip(grad, np.min(grad)/2, np.max(grad)/2)
+            grad = np.max(grad, -1)
+    
+            visualizations.append(grad)
 
-# Load the image
-img = np.load(args.preprocessed_img)
-batch = np.array([img])
-unprocessed_img = utils.load_img(args.unprocessed_img)
-
-# Load the model
-model = load_model(args.model)
-
-# Make predictions (will be used later)
-pred = model.predict(np.array([img]))[0]
-
-# Swap softmax with linear
-model.layers[-1].activation = activations.linear
-model = utils.apply_modifications(model)
-
-# Get visualization of last layer
-layer_idx = -1
-visualizations = []
-neurons = model.layers[layer_idx].output_shape[1]
-
-# For each output option, visualize which inputs effect it.
-
-if args.vis == 'cam':
+    # Plot the visualizations for each output option.
+    f, ax = plt.subplots(2, neurons)
+    
     for i in range(neurons):
-        grads = visualize_cam(model, layer_idx, filter_indices=i,
-                              seed_input=img, penultimate_layer_idx=args.conv_layer,
-                              backprop_modifier=None)
-        # Lets overlay the heatmap onto original image.
-        jet_heatmap = np.uint8(cm.jet(grads)[..., :3] * 255)
-        visualizations.append(overlay(jet_heatmap, unprocessed_img, alpha=0.5))
-elif args.vis == 'saliency':
-    for i in range(neurons):
-        visualizations.append(visualize_saliency(model, layer_idx, backprop_modifier='guided',
-                                                 filter_indices=i, seed_input=img))
-elif args.vis == 'shap':
-    # select a set of background examples to take an expectation over
-    # NOTE: Not sure what background image to use, an expectation or a white image.
-    # Trying white image for now.
-    assert args.background, "Shap uses a background image"
-    background = np.load(args.background)
-    if len(background.shape) == 3:
-        background = np.array([background])
+        ax[i, 0].set_title("Neuron {} activation: %.6f".format(i) % pred[i])
+        ax[i, 0].imshow(unprocessed_img)
+        ax[i, 1].set_title("Importance map for neuron {}:".format(i))
+        heatmap = ax[i, 1].imshow(visualizations[i])
+        f.colorbar(heatmap, ax=ax[i, 1])
 
-    # explain predictions of the model on four images
-    e = shap.DeepExplainer(model, background)
+    print("Saving Figure to {}_explained_{}.png".format(name, vis))
+    plt.savefig('{}_explained_{}.png'.format(name, vis))
+    
+    if show:
+        plt.show()
 
-    shap_values = e.shap_values(batch)
 
-    shap_values[0] = shap_values[0][0].astype('float32')
-    shap_values[1] = shap_values[1][0].astype('float32')
+if __name__ == '__main__':
+    plt.rcParams['figure.figsize'] = (15, 10)
 
-    # normalize the arrays
-    shap_values = shap_values / (np.max(shap_values) - np.min(shap_values))
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--model', type=str, help='path of keras model')
+    parser.add_argument('--unprocessed_img', type=str, help='path of raw image.')
+    parser.add_argument('--preprocessed_img', type=str, help='path of image. Must be npy file and preprocessed.')
+    parser.add_argument('--vis', type=str, default='saliency', help='either CAM or saliency')
+    parser.add_argument('--conv_layer', type=int, default=None,
+                        help='Used for CAM. Index of last convolutional layer')
+    parser.add_argument('--background', type=str, help='path of "background" image')
+    parser.add_argument('--show', type=bool, default=False, help='whether to display the image')
+    # TODO: There seems to be a bug with show always showing.
 
-    # plot the feature attributions
-    visualizations = shap_values
+    args = parser.parse_args()
 
-elif args.vis == 'integrated_grad':
-    for i in range(neurons):
-        assert args.background, "Integrated Gradients uses a background image"
+    assert args.vis in ['cam', 'saliency', 'shap', 'integrated_grad'], \
+        "vis must be cam, shap, integrated_grad or saliency!"
+
+    # Load the image
+    preprocessed_img = np.load(args.preprocessed_img)
+    unprocessed_img = utils.load_img(args.unprocessed_img)
+
+    # Load the background if there is one
+    # Note: Background must be preprocessed!
+    if args.background:
         background = np.load(args.background)
+    else:
+        background = None
 
-        def get_pred_and_grad(input, class_index):
-            input = np.array(input)
-            pred = model.predict(input)
-            fn = K.function([model.input], K.gradients(model.output[:, class_index], model.input))
-
-            grad = fn([input])[0]
-
-            return pred, grad
-
-
-        attributions = integrated_gradients(
-            img,
-            i,
-            get_pred_and_grad,
-            baseline=background,
-            steps=50
-        )
-
-        grad = attributions[0]
-
-        # removing all negative gradients
-        grad = np.clip(grad, 0, np.max(grad))
-        grad = np.max(grad, -1)
-
-        visualizations.append(grad)
-
-# Plot the visualizations for each output option.
-f, ax = plt.subplots(2, neurons)
-
-for i in range(neurons):
-    ax[i, 0].set_title("Neuron {} activation: %.6f".format(i) % pred[i])
-    ax[i, 0].imshow(unprocessed_img)
-    ax[i, 1].set_title("Importance map for neuron {}:".format(i))
-    heatmap = ax[i, 1].imshow(visualizations[i])
-    f.colorbar(heatmap, ax=ax[i, 1])
-
-print("Saving Figure to " + args.unprocessed_img[:-4] + '_explained.png')
-plt.savefig(args.unprocessed_img[:-4] + '_explained.png')
-
-if args.show:
-    plt.show()
+    visualize(preprocessed_img, unprocessed_img, args.vis, args.unprocessed_img[:-4],
+              conv_layer=args.conv_layer, background=background, show=args.show)
